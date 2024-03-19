a1838e6090c1051dd614dbc0cb68907f588bcb60
desafio-7-dnc <agenciadigitalupbr@gmail.com>
2024-03-18 21:13:46 -0300

    first commit

--- src/module/aluno/aluno.model.d.ts
+++ src/module/aluno/aluno.model.d.ts
@@ -3,18 +3,18 @@ import { Aluno, AlunoCreate } from './aluno';
 
 export class Aluno {
 	private db: Knex
-	
-constructor (KnexServide : KnexService) {
-this.db = KnexService.obterConexao() 
-}
+t 
+	constructor(KnexServide: KnexService) {
+		this.db = KnexService.obterConexao()
+	}
 
 
-getAll = async () => {
-return this.db('aluno').select();
-}
+	getAll = async () => {
+		return this.db('aluno').select();
+	}
 
 
-getAll = async (params: any) => {
-return this.db('aluno').insert(params);
-}
+	getAll = async (params: any) => {
+		return this.db('aluno').insert(params);
+	}
 }
